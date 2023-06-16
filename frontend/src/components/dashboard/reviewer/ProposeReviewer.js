e4a8c07b679b64ab43054a0040ab9f1faaca7d35
Navil Rodrigues <rodriguesnavil@gmail.com>
2023-06-16 20:25:13 +0530

    feat: complete propose & vote components for reviewer

--- frontend/src/components/dashboard/reviewer/ProposeReviewer.js
+++ frontend/src/components/dashboard/reviewer/ProposeReviewer.js
@@ -1,11 +1,55 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Box, Button, TextField, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 const ProposeReviewer = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const navigate = useNavigate(); // create a navigate instance
+
+  const onSubmit = (data) => {
+    console.log(data);
+    // add your logic here to handle the form data
+  };
+
   return (
-    <div>
-      <h1>Propose a Reviewer</h1>
-      {/* Add your propose reviewer logic here */}
-    </div>
+    <Box m={3}>
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+      <Typography variant="h5" gutterBottom>
+        Propose a Reviewer
+      </Typography>
+      <Button 
+          variant="outlined" 
+          color="secondary" 
+          onClick={() => navigate('/dashboard')}
+        >
+          Back to Dashboard
+      </Button>
+      </Box>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          {...register('walletAddress', { required: 'Wallet address is required' })}
+          label="Wallet Address"
+          variant="outlined"
+          fullWidth
+          error={!!errors.walletAddress}
+          helperText={errors.walletAddress?.message}
+          margin="normal"
+        />
+        <TextField
+          {...register('description', { required: 'Description is required' })}
+          label="Description (IPFS Link)"
+          variant="outlined"
+          fullWidth
+          error={!!errors.description}
+          helperText={errors.description?.message}
+          margin="normal"
+        />
+        <Button variant="contained" color="primary" type="submit">
+          Propose
+        </Button>
+      </form>
+    </Box>
   );
 };
 
