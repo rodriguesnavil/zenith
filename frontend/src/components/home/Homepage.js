d7f1f3eca868de060e3351f7ce1436122632d188
Navil Rodrigues <rodriguesnavil@gmail.com>
2023-06-18 07:13:34 +0530

    refactor: bring homepage content to centre of screen

--- frontend/src/components/home/Homepage.js
+++ frontend/src/components/home/Homepage.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: {
+    h1: {
+      fontSize: '2.8rem',
+      fontWeight: 500,
+      color: '#3f51b5', // change the color as per your requirement
+    },
+    body1: {
+      fontSize: '1.5rem',
+      color: '#616161', // change the color as per your requirement
+    },
+  },
+});
 
 const Homepage = () => {
   return (
-    <div>
-      <h1>Welcome to Zenith</h1>
-      <p>Decentralizing the academic publishing process...</p>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        height="100vh"
+        padding={3}
+      >
+        <Typography variant="h1" gutterBottom>
+          Welcome to Zenith
+        </Typography>
+        <Typography variant="body1">
+          Decentralizing the academic publishing process...
+        </Typography>
+      </Box>
+    </ThemeProvider>
   );
 };
 
