f78b6f26382511feb60dd3ce1efa418c5be308fd
Navil Rodrigues <rodriguesnavil@gmail.com>
2023-06-18 07:20:29 +0530

    refactor: select role to center of screen

--- frontend/src/components/auth/RoleSelect.js
+++ frontend/src/components/auth/RoleSelect.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-import { FormControl, InputLabel, Select, MenuItem, Button } from '@mui/material';
+import { FormControl, InputLabel, Select, MenuItem, Button, Typography, Box, Paper } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { setUserRole } from '../../helpers/EthereumHelper';
 import { UserContext } from '../../contexts/UserContext';
@@ -22,22 +22,43 @@ const RoleSelect = () => {
   };
 
   return (
-    <div>
-      <FormControl fullWidth>
-        <InputLabel id="role-select-label">Role</InputLabel>
-        <Select
-          labelId="role-select-label"
-          id="role-select"
-          value={role}
-          onChange={handleChange}
-        >
-          <MenuItem value={"author"}>Author</MenuItem>
-          <MenuItem value={"reviewer"}>Reviewer</MenuItem>
-          <MenuItem value={"editor"}>Editor</MenuItem>
-        </Select>
-      </FormControl>
-      <Button variant="contained" color="primary" onClick={confirmRole}>Confirm Role</Button>
-    </div>
+    <Box 
+      display="flex" 
+      flexDirection="column" 
+      justifyContent="center" 
+      alignItems="center" 
+      height="100vh"
+      padding={2}
+    >
+      <Typography variant="h4" gutterBottom align="center">
+        Please Select Your Role
+      </Typography>
+      <Box maxWidth="500px">
+        <Typography variant="body1" gutterBottom align="justify">
+          Choose the role that you will be using in this academic publishing process.
+        </Typography>
+      </Box>
+      <Paper elevation={3} style={{ padding: '20px', width: '100%', maxWidth: '500px' }}>
+        <FormControl fullWidth>
+          <InputLabel id="role-select-label">Role</InputLabel>
+          <Select
+            labelId="role-select-label"
+            id="role-select"
+            value={role}
+            onChange={handleChange}
+          >
+            <MenuItem value={"author"}>Author</MenuItem>
+            <MenuItem value={"reviewer"}>Reviewer</MenuItem>
+            <MenuItem value={"editor"}>Editor</MenuItem>
+          </Select>
+        </FormControl>
+        <Box mt={2}>
+          <Button variant="contained" color="primary" onClick={confirmRole} fullWidth>
+            Confirm Role
+          </Button>
+        </Box>
+      </Paper>
+    </Box>
   );
 };
 
